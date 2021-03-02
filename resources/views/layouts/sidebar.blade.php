1abf9ae7bb3ed9bfd17aa265866ba39da99b4a75
marcellwr <52841685+marcellwr@users.noreply.github.com>
2021-03-03 03:34:02 +0700

    Initial commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,17 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="{{ route('home') }}" class="brand-link">
+        <img src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop"
+             alt="{{ config('app.name') }} Logo"
+             class="brand-image img-circle elevation-3">
+        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+    </a>
+
+    <div class="sidebar">
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                @include('layouts.menu')
+            </ul>
+        </nav>
+    </div>
+
+</aside>
